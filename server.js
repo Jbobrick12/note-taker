82729b1042edd943fc56db8ba1d6b2a2be08c2c0
Justin Bobrick <j.bobrick@outlook.com>
2023-08-31 23:05:42 -0700

    i thought i knew what i was doing

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(express.static("public"));
 
 //  HTML routess
-app.get("/notes", (req, res) => {
-  res.sendFile(__dirname + "/public/notes.html");
-});
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
 });
 
 // API routes
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
-      console.error(err);
+      console.log(response);
     } else {
       const notes = JSON.parse(data);
       res.json(notes);
@@ -33,21 +34,20 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
-      console.error(err);
+      console.log(response);
     } else {
       const notes = JSON.parse(data);
       const newNote = req.body;
 
-      //   Generates a unique ID for the note
       newNote.id = uuidv4();
 
-      // Adds the notes to the array
+      // Adds the notes
       notes.push(newNote);
 
-      // This saves the notes back to the file
+      // saves the notes back to the file
       fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
         if (err) {
-          console.error(err);
+          console.log(response);
         } else {
           res.json(newNote);
         }
